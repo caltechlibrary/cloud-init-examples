80990bea0bd9759c1f3257a1c1c62c32a5b5cc1d
R. S. Doiel <rsdoiel@gmail.com>
2022-05-12 16:35:49 -0700

    Quick Save

--- staff-favorites.bash
+++ staff-favorites.bash
@@ -4,6 +4,6 @@
 # This file adds some Staff favorites developer tools and 
 # optimization including, e.g. emacs, tcsh.
 #
-sudo apt vim
+sudo apt install vim
 sudo apt install emacs
 sudo apt install tcsh
