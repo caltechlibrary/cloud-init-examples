4e8b61f9f6b8355c4dfce19e413f44bc9bab833f
R. S. Doiel <rsdoiel@gmail>
2024-06-18 15:25:31 -0700

    almost got a turn key 'local' install working for RDM in multipass

--- invenio_rdm_machine.bash
+++ invenio_rdm_machine.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+INSTANCE_NAME="invenio-rdm"
+
 #
 # This script generates a "RDM Machine" using Multipass.
 #
@@ -8,54 +10,65 @@
 # the upcoming v12 release yet (expecting an announcement at 
 # Open Repositories, June, 2024.
 #
-echo "Launch invenio-rdm"
+echo "Launch ${INSTANCE_NAME}"
 if ! multipass launch jammy \
     --verbose \
-    --name invenio-rdm \
+    --name "${INSTANCE_NAME}" \
     --memory 8G \
     --disk 150G \
     --cpus 2 \
     --cloud-init invenio-rdm.yaml; then
     echo
-    echo "Failed to create invenio-rdm."
+    echo "Failed to create ${INSTANCE_NAME}."
     echo
     exit 1
 fi
-if ! multipass info invenio-rdm; then
+if ! multipass info "${INSTANCE_NAME}"; then
     echo
-    echo 'failed to create invenio-rdm, aborting'
+    echo 'failed to create ${INSTANCE_NAME}, aborting'
     echo
     exit 1
 fi
-if ! multipass exec invenio-rdm -- /usr/local/bin/install_rdm_nvm.bash; then
+echo "Installing RDM required software"
+if ! multipass exec "${INSTANCE_NAME}" -- /usr/local/bin/install_rdm_nvm.bash; then
     echo
     echo 'WARNING: failed to to install nvm'
     echo 'Access your VM and run /usr/local/bin/install_rdm_nvm.bash'
     echo 'Access your VM and run /usr/local/bin/install_rdm_node.bash'
     echo
-elif ! multipass exec invenio-rdm -- /usr/local/bin/install_rdm_node.bash; then
+    exit 1
+fi
+if ! multipass exec "${INSTANCE_NAME}" -- /usr/local/bin/install_rdm_node.bash; then
     echo
     echo 'WARNING: failed to to install, nvm, nodejs and npm'
     echo 'Access your VM and run /usr/local/bin/install_rdm_node.bash'
     echo
-elif ! multipass restart invenio-rdm; then
+    exit 1
+fi
+if ! multipass restart "${INSTANCE_NAME}"; then
     # Finally we need to restart the machine. 
     echo
     echo 'WARNING: restart failed, check logs on VM restart'
     echo
+    exit 1
 fi
-RDM_IP_ADDRESS=$(multipass list | grep invenio-rdm | cut -c 43-58)
+
+# FIXME: This is where I would want to map the instance name to the 
+# RDM instance and run setup_rdm_instance.bash remotely passing in the 
+# Instance name and version id.
+
+RDM_IP_ADDRESS=$(multipass list | grep "${INSTANCE_NAME}" | cut -c 43-58)
 cat <<EOT
 
   Welcome to the RDM Machine. You can access if with
 
-      multipass shell invenio-rdm
+      multipass shell ${INSTANCE_NAME}
 
   There are some additional steps to take to get RDM working
   on your virtual machine.  If you want a vanilla instance
   run 
 
-    setup_rdm_instance.bash v11.0
+    setup_rdm_instance.bash INSTANCE_NAME v11.0
 
   If you want to run a copy of CaltechAUTHORS run
 
